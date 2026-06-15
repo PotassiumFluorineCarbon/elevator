867936bba09e8a1aac302ec68080458b122e70d2
Eric Eigler <eric@elastic.org>
2026-06-15 15:33:25 -0400

    moved elevatoroperator to main

--- software/rpi/Desktop/ElevatorDemoLoop/src/main.cpp
+++ software/rpi/Desktop/ElevatorDemoLoop/src/main.cpp
@@ -12,6 +12,129 @@ using namespace std;
 
 // ******************************************************************
 
+void elevatoroperator() {
+	enum State state = initial;
+	TPCANMsg Rxmsg;
+	HANDLE h2;
+
+	// Open a CAN channel 
+	h2 = LINUX_CAN_Open("/dev/pcanusb32", O_RDWR);
+
+	// Initialize an opened CAN 2.0 channel with a 125kbps bitrate, accepting standard frames
+	status = CAN_Init(h2, CAN_BAUD_125K, CAN_INIT_TYPE_ST);
+
+	// Clear the channel - new - Must clear the channel before Tx/Rx
+	status = CAN_Status(h2);
+
+	while (1) {
+		while ((status = CAN_Read(h2, &Rxmsg)) == PCAN_RECEIVE_QUEUE_EMPTY) {//read message from CAN bus
+			// No message received, continue waiting
+
+		}
+
+		switch (state) {
+		case initial:
+			// Initialize elevator
+			if (Rxmsg.ID == ID_EC_TO_ALL && Rxmsg.DATA[0] == AT_FLOOR1) {
+				state = waiting_at_1;
+			}
+			else if (Rxmsg.ID == ID_EC_TO_ALL && Rxmsg.DATA[0] == AT_FLOOR2) {
+				state = waiting_at_2;
+			}
+			else if (Rxmsg.ID == ID_EC_TO_ALL && Rxmsg.DATA[0] == AT_FLOOR3) {
+				state = waiting_at_3;
+			}
+			break;
+		case waiting_at_1:
+			// Waiting at floor 1
+			// If command received, state = moving_to_2 or 3
+			if (Rxmsg.ID == 0x202 && Rxmsg.DATA[0] == 0x01) {  // FC2 is calling
+				// now send message to EC to MOVE TO FLOOR 2: CAN ID 0X100, MESSAGE BYTE 0X06
+				sendMsg(0x100, 0x06, h2);
+				state = moving_to_2;
+			}
+			if (Rxmsg.ID == 0x200 && Rxmsg.DATA[0] == 0x02) {
+				sendMsg(0x100, 0x06, h2)
+					state = moving_to_2;
+			}
+
+			if (Rxmsg.ID == 0x203 && Rxmsg.DATA[0] == 0x01) {
+				sendMsg(0x100, 0x07, h2);
+				state = moving_to_3;
+			}
+
+			if (Rxmsg.ID == 0x200 && Rxmsg.DATA[0] == 0x03) {
+				sendMsg(0x100, 0x07, h2);
+				state = moving_to_3;
+			}
+			break;
+		case moving_to_1:
+			if (Rxmsg.ID == ID_EC_TO_ALL && Rxmsg.DATA[0] == AT_FLOOR1) {
+				state = waiting_at_1;
+			}
+
+			break;
+		case waiting_at_2:
+			if (Rxmsg.ID == 0x201 && Rxmsg.DATA[0] == 0x01) {
+				sendMsg(0x100, 0x05, h2);
+				state = moving_to_1;
+			}
+			if (Rxmsg.ID == 0x200 && Rxmsg.DATA[0] == 0x01) {
+				sendMsg(0x100, 0x05, h2)
+					state = moving_to_1;
+			}
+
+			if (Rxmsg.ID == 0x203 && Rxmsg.DATA[0] == 0x01) {
+				sendMsg(0x100, 0x07, h2);
+				state = moving_to_3;
+			}
+
+			if (Rxmsg.ID == 0x200 && Rxmsg.DATA[0] == 0x03) {
+				sendMsg(0x100, 0x07, h2);
+				state = moving_to_3;
+			}
+			break;
+		case moving_to_2:
+			if (Rxmsg.ID == ID_EC_TO_ALL && Rxmsg.DATA[0] == AT_FLOOR2) {
+				state = waiting_at_2;
+			}
+
+			break;
+		case waiting_at_3:
+			if (Rxmsg.ID == 0x201 && Rxmsg.DATA[0] == 0x01) {
+				sendMsg(0x100, 0x05, h2);
+				state = moving_to_1;
+			}
+			if (Rxmsg.ID == 0x200 && Rxmsg.DATA[0] == 0x01) {
+				sendMsg(0x100, 0x05, h2)
+					state = moving_to_1;
+			}
+
+			if (Rxmsg.ID == 0x202 && Rxmsg.DATA[0] == 0x01) {
+				sendMsg(0x100, 0x06, h2);
+				state = moving_to_2;
+			}
+
+			if (Rxmsg.ID == 0x200 && Rxmsg.DATA[0] == 0x02) {
+				sendMsg(0x100, 0x06, h2);
+				state = moving_to_2;
+			}
+
+			break;
+		case moving_to_3:
+			if (Rxmsg.ID == ID_EC_TO_ALL && Rxmsg.DATA[0] == AT_FLOOR3) {
+				state = waiting_at_3;
+			}
+
+			break;
+		case fault:
+			// Handle fault condition (e.g. stop elevator, sound alarm, etc.)
+			break;
+		}
+		printf("\nState: ", state);
+	}
+}
+
 int main() {
 
 	int choice; 
