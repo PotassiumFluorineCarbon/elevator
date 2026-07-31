04ad31a8ca5e23bb835c8f6d032b87164a58f983
Eric <eric@elastic.org>
2026-07-30 20:20:19 -0400

    fixed spacing

--- php/add_request.php
+++ php/add_request.php
@@ -5,74 +5,32 @@ ini_set('display_errors', 1);
 
 include "database.php";
 
-
-if(isset($_POST["floor"]))
-{
+if (isset($_POST["floor"])) {
     $floor = intval($_POST["floor"]);
 
-    $sql = "
-    INSERT INTO requests
-    (
-        floor,
-        direction,
-        processed
-    )
-    VALUES
-    (
-        ?,
-        NULL,
-        FALSE
-    )
-    ";
+    $sql = "INSERT INTO requests (floor, direction, processed)
+            VALUES (?, NULL, FALSE)";
 
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $floor);
-
-}
-
-
-else if(isset($_POST["direction"]))
-{
+} elseif (isset($_POST["direction"])) {
     $direction = $_POST["direction"];
 
-    $sql = "
-    INSERT INTO requests
-    (
-        floor,
-        direction,
-        processed
-    )
-    VALUES
-    (
-        NULL,
-        ?,
-        FALSE
-    )
-    ";
+    $sql = "INSERT INTO requests (floor, direction, processed)
+            VALUES (NULL, ?, FALSE)";
 
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $direction);
-
-}
-
-
-else
-{
+} else {
     die("No request received");
 }
 
-
-if($stmt->execute())
-{
+if ($stmt->execute()) {
     echo "Request added";
-}
-else
-{
+} else {
     echo "Error: " . $stmt->error;
 }
 
-
 $stmt->close();
 $conn->close();
-
 ?>
